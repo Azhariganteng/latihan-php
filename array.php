a934060b846b843464b5706e743ad6514e4dcd60
azhariganteng <azhariramadan59@gmail.com>
2022-08-26 10:05:54 +0700

    telah selesai array

--- array.php
+++ array.php
@@ -14,9 +14,53 @@ $produk = [
 // echo $produk["total_produk"];
 
 $data_barang = [
-    ["barang" => "HNI", "harga" => "2000", "stok" => "999999999"],
-    ["barang" => "Kopi", "harga" => "2000", "stok" => "999999999"],
+
+    'data'=>["barang" => "HNI", "harga" => "2000", "stok" => "999999999"],
+    'barang'=>["barang" => "Kopi", "harga" => "2000", "stok" => "999999999"],
 ];
 
-print_r($data_barang[1]["barang"]);
+// print_r($data_barang["data"]["barang"]);
+
+$data_barang = [
+
+    ["barang" => "HNI", "harga" => 3000, "stok" => "999999999"],
+    ["barang" => "HNI", "harga" => 3000, "stok" => "999999999"],
+    [],
+];
+
+//PERULANGAN FOR BIASA
+for ($index=0; $index<6;) {
+    if ($index==2) {
+        echo 'ini key ke' .$index . PHP_EOL; 
+        //break;
+    }else {
+    echo 'ini index ke' .$index . PHP_EOL;
+    }
+    $index++;
+}
+
+//PERULANGAN ARRAY MENGGUNAKAN FOR
+echo count($data_barang);
+
+for ($exp=0; $exp<count($data_barang); $exp++) {
+    echo 'ini nama barang : ' .$data_barang [$exp]["barang"] . PHP_EOL;
+}
+
+//PERULANGAN ARRAY MENGGUNAKAN FOREACH
+foreach ($data_barang as $item) {
+    echo $item ["harga"] . PHP_EOL;
+}
+
+$new_data_barang = (array_map(function ($item) {
+    if ($item ["harga"]> 2000) {
+        return $item ["harga"];
+    }
+}, $data_barang)) ;
+(array_filter($data_barang, function ($item) {
+    if ($item ["harga"]> 2000) {
+        return $item ["harga"];
+    }
+}));
+print_r (($data_barang));
 ?> 
+
